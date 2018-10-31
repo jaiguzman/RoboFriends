041a069bb2b300dc14bfa767a8227e72b72f086d
Jaime G <jaime.guzman92@gmail.com>
2018-10-31 15:07:14 -0700

    Updates

---
+++ precache-manifest.24e9b06825b8d95cf9b06e02cf2d2734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "877cac2a9c44d7342a3d",
+    "url": "/RoboFriends/static/js/runtime~main.877cac2a.js"
+  },
+  {
+    "revision": "8d981768bba14374a3cb",
+    "url": "/RoboFriends/static/js/main.8d981768.chunk.js"
+  },
+  {
+    "revision": "36182d073130aacd2a6b",
+    "url": "/RoboFriends/static/js/1.36182d07.chunk.js"
+  },
+  {
+    "revision": "8d981768bba14374a3cb",
+    "url": "/RoboFriends/static/css/main.2a0854c0.chunk.css"
+  },
+  {
+    "revision": "36182d073130aacd2a6b",
+    "url": "/RoboFriends/static/css/1.0b222c10.chunk.css"
+  },
+  {
+    "revision": "424c885826caa79b12959efa7ac189a7",
+    "url": "/RoboFriends/index.html"
+  }
+];
